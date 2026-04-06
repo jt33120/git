a0ad40f628d9ceafb06ed6cb0db7d7aa75e75684
JulianTalou <julian.talou@polytechnique.edu>
2026-04-06 22:22:34 +0200

    Fix backend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,12 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "https://git-alpha-hazel.vercel.app", "http://localhost:3000"],
+    allow_origins=[
+        settings.frontend_url,
+        "https://git-alpha-hazel.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
